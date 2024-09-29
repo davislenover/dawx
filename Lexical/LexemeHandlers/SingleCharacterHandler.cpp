90ee8a0b89f6bf02a14a81c60b63eceeff67614a
Davis <davislenover02@gmail.com>
2024-09-28 20:37:15 -0400

    Added basic identifying single character token logic

--- Lexical/LexemeHandlers/SingleCharacterHandler.cpp
+++ Lexical/LexemeHandlers/SingleCharacterHandler.cpp
@@ -1,1 +1,29 @@
 #include "SingleCharacterHandler.h"
+#include "../../FileManagement/CodeCharacter.h"
+
+TokenType SingleCharacterHandler::getIDTokenType(int tokenIndex) {
+    return this->generatedTokens[tokenIndex];
+}
+
+Token SingleCharacterHandler::generateToken(CodeCharacter curCharacter, CodeStream stream) {
+    char character = curCharacter.getCharacter();
+    if (character == '(') {
+        return Token(TokenType::LEFT_PAREN,"(",curCharacter.getOffset());
+    } else if (character == ')') {
+        return Token(TokenType::RIGHT_PAREN,")",curCharacter.getOffset());
+    } else if (character == '{') {
+        return Token(TokenType::LEFT_CURLY_BRACE,"{",curCharacter.getOffset());
+    } else if (character == '}') {
+        return Token(TokenType::RIGHT_CURLY_BRACE,"}",curCharacter.getOffset());
+    } else if (character == '[') {
+        return Token(TokenType::LEFT_SQUARE_BRACKET,"[",curCharacter.getOffset());
+    } else if (character == ']') {
+        return Token(TokenType::RIGHT_SQUARE_BRACKET,"]",curCharacter.getOffset());
+    } else if (character == ';') {
+        return Token(TokenType::SEMICOLON,";",curCharacter.getOffset());
+    } else if (character == '.') {
+        return Token(TokenType::PERIOD,".",curCharacter.getOffset());
+    } else if (character == ',') {
+        return Token(TokenType::COMMA,",",curCharacter.getOffset());
+    }
+}
--- Lexical/LexemeHandlers/SingleCharacterHandler.h
+++ Lexical/LexemeHandlers/SingleCharacterHandler.h
@@ -1,5 +1,11 @@
 #include "LexemeHandler.h"
 
 class SingleCharacterHandler : public LexemeHandler {
-
+private:
+    TokenType generatedTokens[9] = {TokenType::LEFT_PAREN,TokenType::RIGHT_PAREN, TokenType::LEFT_CURLY_BRACE,
+                                    TokenType::RIGHT_CURLY_BRACE, TokenType::LEFT_SQUARE_BRACKET, TokenType::RIGHT_SQUARE_BRACKET,
+                                    TokenType::SEMICOLON, TokenType::PERIOD, TokenType::COMMA};
+public:
+    TokenType getIDTokenType(int tokenIndex = 0) override;
+    Token generateToken(CodeCharacter curCharacter, CodeStream stream) override;
 };
